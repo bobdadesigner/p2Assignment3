4de9efdf1628f9edc70c20e18adf69c6417ede3a
Bob Chang <bobdadesigner@gmail.com>
2018-10-04 15:15:37 +1300

    drawLine

--- JavaApplication14/src/Line.java
+++ JavaApplication14/src/Line.java
@@ -1,14 +1,7 @@
 
-import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Point;
-import java.awt.Toolkit;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
-import java.util.ArrayList;
-import javax.swing.JFrame;
+
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -21,46 +14,20 @@ import javax.swing.JFrame;
  */
 public class Line extends Shape {
 
-    private Shape line;
-    
+//    private Shape line;
     /**
      * @param args the command line arguments
      */
-    public Line(Point p) {
-        super(p);
+    public Line(Point startPoint) {
+        super(startPoint);
 
-//        setPreferredSize(new Dimension(500, 400));
-//        setBackground(Color.WHITE);
-        
     }
 
     @Override
     public void draw(Graphics g) {
-//        super.paintComponent(g);
-
         g.setColor(this.getColour());
+        g.drawLine(startPoint.x, startPoint.y, controlPoint.x, controlPoint.y);
 
-//        line.draw(g);
-// g.drawLine(25, 25, 120, 120);
-            
-            g.drawLine(line.startPoint.x, line.startPoint.y, line.controlPoint.x, line.controlPoint.y);
-//        g.drawLine(this.startPoint.x, this.startPoint.y, this.controlPoint.x, this.controlPoint.y);
-    }
-
-    public static void main(String[] args) {
-
-        Line a = new Line(new Point());
-
-
-        JFrame frame = new JFrame("Following Eyes"); //create frame to hold our JPanel subclass	
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().add(a);  //add instance of MyGUI to the frame
-        frame.pack(); //resize frame to fit our Jpanel
-        Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
-//        frame.setPreferredSize(new Dimension(1000,1000));
-        frame.setLocation(new Point((d.width / 2) - (frame.getWidth() / 2), (d.height / 2) - (frame.getHeight() / 2)));
-        //show the frame	
-        frame.setVisible(true);
     }
 
 }
--- JavaApplication14/src/Shape.java
+++ JavaApplication14/src/Shape.java
@@ -1,10 +1,7 @@
 
-
-
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Point;
-import javax.swing.JPanel;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -15,58 +12,40 @@ import javax.swing.JPanel;
  *
  * @author xuchang
  */
-public abstract class Shape extends JPanel{
-    
+public abstract class Shape {
+
     protected Point startPoint;
     protected Point controlPoint;
     private Color colour;
-    
+
     public Shape() {
-       this.startPoint = new Point(0,0);
-        
+        this.startPoint = new Point(0, 0);
+        this.controlPoint = new Point(0, 0);
+
     }
-    
+
     public Shape(Point startPoint) {
         this.startPoint = startPoint;
-        
+        this.controlPoint = startPoint;
     }
-    
+
     public Color getColour() {
         return colour;
     }
-    
+
     public void setColour(Color colour) {
         this.colour = colour;
     }
-    
+
     public void setControlPoint(Point controlPoint) {
         this.controlPoint = controlPoint;
     }
-    
+
     public abstract void draw(Graphics g);
-    
+
     public String toString() {
-        
-       
+
         return " startPoint " + startPoint.toString() + " controlPoint " + controlPoint.toString() + " colour " + colour.toString();
-    }   
-    
-    public static void main(String[] args){
-        Point p = new Point(10,30);
-        Shape a = new Shape() {
-            @Override
-            public void draw(Graphics g) {
-                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-            }
-        };
-        a.setColour(Color.blue);
-        a.setControlPoint(p);
-        
-        
-        System.out.println(a.toString());
-        System.out.println();
-        
-        
-        
     }
+
 }
